15ad72aaca40250e147edef66b40b07a964977ea
Douglas Smith <mcdoogen@gmail.com>
2022-05-07 13:34:48 -0400

    some navigation added

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-            appBar: AppBar(title: const Text('Hello!')),
-            body: const Center(child: Text('Hello!'))));
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Page1(),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Hello!')),
+      body: const Center(child: Text('Hello!')),
+      drawer: Drawer(
+        child: ListView(children: [
+          const DrawerHeader(child: Center(child: Text('Hello!'))),
+          ListTile(
+              leading: const Icon(Icons.star),
+              title: const Text('Star!'),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Page2()));
+              }),
+          const ListTile(),
+          const ListTile(),
+        ]),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text("Return!")));
   }
 }
