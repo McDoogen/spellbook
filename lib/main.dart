3c6f206cf1f1a615ce244fe0961ecb6c87723363
Douglas Smith <mcdoogen@gmail.com>
2022-05-18 15:25:27 -0400

    More functional Recipe Book

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,113 @@ import 'package:flutter/material.dart';
 
 import 'package:spellbook/spell_db.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const RecipeBook());
+
+/**
+ * Widget Structure
+ * Recipebook - MaterialApp>Scaffold, stateless; root widget for entire project
+ * RecipeCategoryList - Drawer, stateful?; contains selectable list of categories
+ * RecipeListView - ListView, stateful; contains list of recipes to choose from
+ * RecipeDetailView - Contains contents of a recipe
+ * 
+ * 
+ * Editing?
+ * Adding new?
+ */
+
+class RecipeBook extends StatelessWidget {
+  const RecipeBook({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Recipe Book',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(title: const Text('TEMPORARY APPBAR TITLE')),
+          body: const RecipeListView(),
+          drawer: const RecipeCategoryList(),
+        ));
+  }
+}
+
+class RecipeCategoryList extends StatefulWidget {
+  const RecipeCategoryList({Key? key}) : super(key: key);
+
+  @override
+  State<RecipeCategoryList> createState() => _RecipeCategoryListState();
+}
+
+class _RecipeCategoryListState extends State<RecipeCategoryList> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(children: [
+        const DrawerHeader(child: Text('TEMPORARY HEADER')),
+        ListTile(
+            leading: const Icon(Icons.star),
+            title: const Text('TEMPORARY TITLE 1'),
+            onTap: () {}),
+        ListTile(
+            leading: const Icon(Icons.star),
+            title: const Text('TEMPORARY TITLE 2'),
+            onTap: () {}),
+      ]),
+    );
+  }
+}
+
+class RecipeListView extends StatefulWidget {
+  const RecipeListView({Key? key}) : super(key: key);
+
+  @override
+  State<RecipeListView> createState() => _RecipeListViewState();
+}
+
+class _RecipeListViewState extends State<RecipeListView> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(children: [
+      ListTile(
+          leading: const Icon(Icons.star),
+          title: const Text('TEMPORARY TITLE 1'),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const RecipeDetailView()));
+          }),
+      ListTile(
+          leading: const Icon(Icons.star),
+          title: const Text('TEMPORARY TITLE 2'),
+          onTap: () {}),
+    ]);
+  }
+}
+
+class RecipeDetailView extends StatefulWidget {
+  const RecipeDetailView({Key? key}) : super(key: key);
+
+  @override
+  State<RecipeDetailView> createState() => _RecipeDetailViewState();
+}
+
+class _RecipeDetailViewState extends State<RecipeDetailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('TEMPORARY BUTTON')),
+    );
+  }
+}
+
+// Everything below is testing :D
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
